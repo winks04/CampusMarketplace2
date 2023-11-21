449e64da3c32068613ad58edc379c577a0b66b0f
winks04 <150486104+winks04@users.noreply.github.com>
2023-11-21 13:42:48 -0500

    fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,12 +35,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
-        compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
-    }
-
 }
 
 dependencies {
@@ -52,12 +47,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.compose.material:material")
-    // Android Studio Preview support
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-
-    // UI Tests
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
