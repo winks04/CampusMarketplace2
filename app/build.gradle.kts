51c302aefba8352a702d79d91ed04f1866174dcc
winks04 <150486104+winks04@users.noreply.github.com>
2023-11-28 20:37:33 -0500

    fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,9 +32,19 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        freeCompilerArgs += listOf(
+            "-P",
+            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+        )
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+
     }
 }
 
@@ -43,8 +53,21 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
+    implementation ("androidx.compose.ui:ui:1.0.0")
+    implementation ("androidx.compose.material:material:1.0.0")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//    implementation("androidx.compose.material:material")
+    // Android Studio Preview support
+//    implementation("androidx.compose.ui:ui-tooling-preview")
+//    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
+
+    // UI Tests
+//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+//    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
--- app/src/main/java/com/example/campusmarketplace/MainActivity.kt
+++ app/src/main/java/com/example/campusmarketplace/MainActivity.kt
@@ -1,28 +1,25 @@
 package com.example.campusmarketplace
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.example.campusmarketplace.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+//class MainActivity : AppCompatActivity() {
 //    NavigationView.OnNavigationItemSelectedListner {
 
 //    private lateinit var fragmentManager: FragmentManager
 //    private lateinit var binding: ActivityMainBinding
 //
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
 //        binding = ActivityMainBinding.inflate(layoutInflater)
 //        setContentView(binding.root)
 //
 //        setSupportActionBar(binding.toolbar)
 //
 //        val toggle = ActionBarDrawerToggle(activity: this, binding.drawerLayout, binding.toolbar, R.string)
-    }
+//    }
 //
 //    override fun onNavigationItemSelected(item: MenuItem): Boolean {
 //        when(item.itemId){
@@ -43,4 +40,16 @@ class MainActivity : AppCompatActivity() {
 //        fragmentTransaction.replace(R.id.fragment_container, fragment)
 //        fragmentTransaction.commit()
 //    }
+//}
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Set the content view to the layout file activity_main.xml
+        setContentView(R.layout.activity_main.xml)
+    }
 }
+
